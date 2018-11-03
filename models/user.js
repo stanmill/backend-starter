3255abe0e73dc3a15ae9f3fca95c95252d38d701
stanmill <stanley.pena29@gmail.com>
2018-11-02 20:36:17 -0400

    database input

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const bcrypt = require('bcrypt-nodejs');
+
+//this is the database and contains attributes 
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+        isAlphanumeric: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+    password_hash: {
+      type: DataTypes.STRING,
+    },
+  });
+
+  //encription package 
+  User.beforeCreate((user) =>
+    new sequelize.Promise((resolve) => {
+      bcrypt.hash(user.password_hash, null, null, (err, hashedPassword) => {
+        resolve(hashedPassword);
+      });
+    }).then((hashedPw) => {
+      user.password_hash = hashedPw;
+    })
+  );
+
+
+  return User;
+}
